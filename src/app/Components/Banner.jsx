23c17d3b75b0c4bc0b26564e57f27d27a987d54a
Md. Aminul Islam Shaon <aminul007.ais@gmail.com>
2024-10-09 03:28:38 +0600

    banner section is responsive for all devices and all global css are broke down for all devices

--- src/app/Components/Banner.jsx
+++ src/app/Components/Banner.jsx
@@ -5,16 +5,16 @@ import React from 'react';
 const Banner = () => {
     return (
         <React.Fragment>
-            <div className="slanted-box gradient-background overflow-auto flex">
-                <div className='w-[231px] h-[25px]  bg-[#fff6a1] absolute top-8 rounded-2xl hidden lg:inline' />
+            <div className="slanted-box gradient-background lg:overflow-auto flex justify-center items-center">
+                <div className='w-[231px] h-[25px] bg-[#fff6a1] absolute top-8 rounded-2xl hidden lg:inline' />
                 <div className='w-[710pc] h-[385px] hidden lg:block'></div>
-                <div className='text-center lg:text-end mr-[60px] mt-[-550px] lg:mt-[-150px]'>
-                    <h3 className='text-[72px] lg:text-[100px] font-Montserrat font-black text-[#f87a01]'>History of</h3>
-                    <p className='font-Cheesburga text-[105px] lg:text-[220px] -mb-5'>ALABAY</p>
-                    <p className=' lg:text-[46px] w-[400px] lg:w-[769px] h-[532px] font-Kumbh font-bold text-[#353535] text-center lg:text-end'>The Central Asian Shepherd Dog, also known as Alabay, has been a guardian of livestock and property for centuries. Originating from Central Asia, these dogs are renowned for their courage, strength, and loyalty.</p>
+                <div className='text-center lg:text-end lg:mr-[60px] -mt-[50px] md:mt-[-150px] lg:mt-[-150px]'>
+                    <h3 className='text-[48px] md:text-[72px] lg:text-[100px] font-Montserrat font-black text-[#f87a01]'>History of</h3>
+                    <p className='font-Cheesburga text-[54px] md:text-[105px] lg:text-[220px] -mb-5'>ALABAY</p>
+                    <p className='mt-8 md:mt-0 lg:text-[46px] md:w-[400px] lg:w-[769px] h-[532px] font-Kumbh font-bold text-[#353535] text-center lg:text-end'>The Central Asian Shepherd Dog, also known as Alabay, has been a guardian of livestock and property for centuries. Originating from Central Asia, these dogs are renowned for their courage, strength, and loyalty.</p>
                 </div>
             </div>
-            <Image src={dog1} alt='puppy' className='absolute md:top-[400px]  lg:top-[325px] water-reflection z-[1]' />
+            <Image src={dog1} alt='puppy' className='absolute top-[320px] md:top-[400px] lg:top-[325px] water-reflection z-[1]' />
         </React.Fragment>
     );
 };
--- src/app/Components/MainBackground.jsx
+++ src/app/Components/MainBackground.jsx
@@ -5,7 +5,7 @@ const MainBackground = () => {
     return (
         <div className="h-[100vh] hidden lg:block">
         <Image src={main} alt="" className="fixed left-1/2 transform -translate-x-1/2   w-full object-cover object-center"/>
-        <div className="relative z-0 ">
+        <div className="lg:relative z-0 ">
           <h1 className="text-[50px] lg:text-[148.48px] font-Cheesburga text-center gradient-text fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-[200%] text-nowrap">welcome to alabay world</h1>
           {/* <h1 className="font-Cheesburga text-white text-5xl fixed bottom-36 bg-[#181C27] w-full text-center px-[50px] py-10">
             where the <span className="gradient-text-2">legendary Central Asian Shepherd Dog</span> meets a new-age adventure. <span className="gradient-text-2">Join us</span> in celebrating the <span className="gradient-text-2">strength</span>, <span className="gradient-text-2">loyalty</span>, and <span className="gradient-text-2">heritage</span> of the Alabay breed.
