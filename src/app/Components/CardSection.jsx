ae353f74bc222d1ba607c6fb6848d43638a46612
Md. Aminul Islam Shaon <aminul007.ais@gmail.com>
2024-10-10 01:02:23 +0600

    footer is responsive for all devices

--- src/app/Components/CardSection.jsx
+++ src/app/Components/CardSection.jsx
@@ -3,27 +3,27 @@ import React from 'react';
 
 const CardSection = () => {
     return (
-        <div className='grid grid-cols-2 lg:grid-cols-4 mx-[76px] gap-[19px]'>
-            <div className="col-span-1 bg-gradient-to-b from-[#DBC70C] flex items-center justify-center rounded-tl-[30px] flex-col px-3 py-6">
-                <p className='text-[40px] font-black font-Montserrat text-white text-center leading-[46px]'>Community Building</p>
-                <p className='text-[32px] font-black font-Montserrat text-white'>and Initial Launch</p>
-                <div className="image-background1 w-[377.05px] h-[505px]"/>                
+        <div className='grid grid-cols-2 md:grid-cols-4 mx-[30px] lg:mx-[76px] gap-3 md:gap-[19px]'>
+            <div className="col-span-1 bg-gradient-to-b from-[#DBC70C] flex items-center justify-center rounded-t-[30px] md:rounded-tl-[30px] flex-col py-6">
+                <p className='text-[20px] lg:text-[40px] font-black font-Montserrat text-white text-center lg:leading-[46px]'>Community Building</p>
+                <p className='text-[16px] lg:text-[32px] font-black font-Montserrat text-white text-center'>and Initial Launch</p>
+                <div className="image-background1 w-full h-[200px] md:h-[210px] lg:h-[505px]" />
             </div>
-            <div className="col-span-1 bg-gradient-to-b from-[#00D4D4] flex items-center justify-center flex-col px-3 py-6">
-                <p className='text-[40px] font-black font-Montserrat text-white text-center'>Merchandise</p>
-                <p className='text-[32px] font-black font-Montserrat text-white mb-10'>Store Launch</p>
-                <div className="image-background2 w-[377.05px] h-[505px]"/>
-                
+            <div className="col-span-1 bg-gradient-to-b rounded-t-[30px] md:rounded-none from-[#00d4d4] flex items-center justify-center flex-col py-6">
+                <p className='text-[20px] lg:text-[40px] font-black font-Montserrat text-white text-center '>Merchandise</p>
+                <p className='text-[16px] lg:text-[32px] font-black font-Montserrat text-white mb-10'>Store Launch</p>
+                <div className="image-background2 w-full h-[200px] md:h-[210px] lg:h-[505px]" />
+
             </div>
-            <div className="col-span-1 bg-gradient-to-b from-[#D427FF] flex items-center justify-center flex-col px-3 py-6">
-                <p className='text-[40px] font-black font-Montserrat text-white text-center leading-[46px]'>Community Events</p>
-                <p className='text-[32px] font-black font-Montserrat text-white'>and contests</p>
-                <div className="image-background3 w-[377.05px] h-[505px]"/>                
+            <div className="col-span-1 bg-gradient-to-b rounded-t-[30px] md:rounded-none from-[#D427FF] flex items-center justify-center flex-col py-6">
+                <p className='text-[20px] lg:text-[40px] font-black font-Montserrat text-white text-center lg:leading-[46px]'>Community Events</p>
+                <p className='text-[16px] lg:text-[32px] font-black font-Montserrat text-white'>and contests</p>
+                <div className="image-background3 w-full h-[200px] md:h-[210px] lg:h-[505px]" />
             </div>
-            <div className="col-span-1 bg-gradient-to-b from-[#8F3A3C] flex items-center justify-center rounded-tr-[30px] flex-col px-3 py-6">
-                <p className='text-[40px] font-black font-Montserrat text-white text-center leading-[46px]'>Expansion and New</p>
-                <p className='text-[32px] font-black font-Montserrat text-white'>Features</p>
-                <div className="image-background4 w-[377.05px] h-[505px]"/>                
+            <div className="col-span-1 bg-gradient-to-b from-[#8F3A3C] flex items-center justify-center rounded-t-[30px] md:rounded-tr-[30px] flex-col py-6">
+                <p className='text-[20px] lg:text-[40px] font-black font-Montserrat text-white text-center lg:leading-[46px]'>Expansion and New</p>
+                <p className='text-[16px] lg:text-[32px] font-black font-Montserrat text-white'>Features</p>
+                <div className="image-background4 w-full h-[200px] md:h-[210px] lg:h-[505px]" />
             </div>
         </div>
     );
--- src/app/Components/Footer.jsx
+++ src/app/Components/Footer.jsx
@@ -7,24 +7,24 @@ import icon2 from '../../../public/Image/twitter-logo 1.png';
 
 const Footer = () => {
     return (
-        <div className='my-[400px]'>
-            <h1 className='text-[200px] font-Cheesburga text-[#FFA800] ml-[76px]'>Social Media <br /> Links</h1>
-            <div className='grid grid-cols-5 items-center justify-between mx-[70px]'>
-                <div className='col-span-2 bg-linksBG w-[700px] h-[480px] bg-center justify-start bg-contain relative'>
-                    <div className='absolute flex gap-[35px] items-center left-[40%] top-[104px] transform -translate-x-[50%]'>
-                        <div className='w-[91px] h-[91px] p-[21px] bg-[#353535] rounded-full'>
+        <div className='my-[100px] md:my-[200px] lg:my-[400px]'>
+            <h1 className='text-[48px] md:text-[90px] lg:text-[200px] font-Cheesburga text-[#FFA800] ml-10 lg:ml-[76px]'>Social Media <br /> Links</h1>
+            <div className='grid grid-flow-col-dense grid-cols-1 md:grid-cols-5 items-center justify-between mx-10 lg:mx-[70px]'>
+                <div className='col-span-1 md:col-span-2 bg-linksBG bg-no-repeat w-[300px] lg:w-[700px] h-[200px] lg:h-[480px] bg-center bg-cover top-[310px] md:top-0  relative'>
+                    <div className='absolute flex gap-[15px] lg:gap-[35px] items-center left-[40%] top-[45px] lg:top-[104px] transform -translate-x-[50%]'>
+                        <div className='w-[40px] lg:w-[91px] h-[40px] lg:h-[91px] p-2 lg:p-[21px] bg-[#353535] rounded-full'>
                             <Image src={icon2} alt='Icon-1' className='' />
                         </div>
-                        <p className='col-span-2 text-[40px] text-nowrap font-Montserrat text-white font-black'>Twitter Link</p>
+                        <p className='col-span-2 text-[18px] lg:text-[40px] text-nowrap font-Montserrat text-white font-black'>Twitter Link</p>
                     </div>
-                    <div className='absolute flex gap-[35px] items-center left-[43%] top-[234px] transform -translate-x-[50%]'>
-                        <div className='w-[91px] h-[91px] p-[21px] bg-[#353535] rounded-full'>
+                    <div className='absolute flex gap-[15px] lg:gap-[35px] items-center left-[43%] top-[105px]  lg:top-[234px] transform -translate-x-[50%]'>
+                        <div className='w-[40px] lg:w-[91px] h-[40px] lg:h-[91px] p-2 lg:p-[21px] bg-[#353535] rounded-full'>
                             <Image src={icon1} alt='Icon-2' className='' />
                         </div>
-                        <p className='col-span-2 text-[40px] text-nowrap font-Montserrat text-white font-black'>Telegram Link</p>
+                        <p className='col-span-2 text-[18px] lg:text-[40px] text-nowrap font-Montserrat text-white font-black'>Telegram Link</p>
                     </div>
                 </div>
-                <Image src={dog1} alt='image-2' className='col-span-3 water-reflection4' />
+                <Image src={dog1} alt='image-2' className='col-span-1 md:col-span-3 water-reflection4' />
             </div>
         </div>
     );
