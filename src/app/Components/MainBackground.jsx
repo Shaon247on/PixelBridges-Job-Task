5348446f435ae401abe17ca9f5d08636ad38f2e1
Md. Aminul Islam Shaon <aminul007.ais@gmail.com>
2024-10-10 05:41:46 +0600

    trying to deply

--- src/app/Components/MainBackground.jsx
+++ src/app/Components/MainBackground.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import main from '../../../public/Image/main-background.jpg';
+import main from '../../../public/Image/main-Background.jpg';
 
 const MainBackground = () => {
     return (
